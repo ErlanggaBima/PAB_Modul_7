0f89b5cf9ba4c0b0dc29725123458b589c7c1081
Erlangga Bima <yoshimorsumimura2002@gmail,com>
2023-12-02 13:11:06 +0700

    first commit

---
+++ screens/index.js
@@ -0,0 +1,4 @@
+import AboutScreen from "./about";
+import TaskScreen from "./task-all";
+import TaskCompletedScreen from "./task-complete";
+export { AboutScreen, TaskCompletedScreen, TaskScreen };
